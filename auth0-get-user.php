db201ccae5d540707fb780bdcfb8009ba7d29635
Beta Server <NOT A REAL EMAIL ADDRESS>
2018-06-13 18:30:39 +0000

    Add basic json route with NO authorization

--- auth0-get-user.php
+++ auth0-get-user.php
@@ -11,14 +11,35 @@
  *
  */
 
-function get_user_object_for_auth0($email) {
-  $user = get_user_by('carrington@gmail.com');
-  return $user;
+function get_user_object_for_auth0($query) {
+  $user = get_user_by('email', $query['email']);
+  $auth0_user = array(
+    'user_id' => $user->ID,
+    'username' => $user->data->user_login,
+    'nickname' => $user->data->user_nicename,
+    'email' => $user->data->user_email,
+    'display_name' => $user->data->display_name,
+    'nicename' => $user->data->user_nicename,
+    'first_name' => null,
+    'last_name' => null,
+    'given_name' => null,
+    'family_name' => null
+  );
+  return $auth0_user;
 };
 
+
 add_action( 'rest_api_init', function() {
-  register_rest_route('mw_auth0/v2', '/user/(?P<email>\d+)', array(
-    'methods' => 'GET',
-    'callback' => 'get_user_object_for_auth0'
+  register_rest_route('mw_auth0/v1', '/user/', array(
+    'methods' => 'POST',
+    'callback' => 'get_user_object_for_auth0',
+    'args' => array(
+      'email' => array(
+        'required' => true,
+	'type' => 'string',
+	'decription' => 'The user\'s email address',
+	'format' => 'email'
+      )
+    )
   ) );
 } );
