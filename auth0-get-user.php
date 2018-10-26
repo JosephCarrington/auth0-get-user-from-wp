cb71170ab64c8132035580d26c94f85b2f9b1590
Alpha Server <NOT_A_REAL_EMAIL_ADDRESS@meowwolf.com>
2018-10-26 15:26:56 +0000

    Add new route to upsert user, to be used instead of all those other routes

--- auth0-get-user.php
+++ auth0-get-user.php
@@ -44,6 +44,7 @@ function get_user_object_for_auth0($query) {
             $auth0_id = get_user_meta( $user->ID, $wpdb->prefix . $site_id . '_auth0_id', true );
         }
       }
+	  slack_log($auth0_id);
   }
 
   if(!$auth0_id) return;
@@ -93,9 +94,54 @@ function create_user_for_auth0($query) {
   update_user_meta( $user_id, 'first_name', $query['first_name'] );
   update_user_meta( $user_id, 'last_name', $query['last_name'] );
   return get_user_by_email( $emailToCreate );
-
-
 };
+
+function verify_token($token) {
+	require_once( plugin_dir_path( __FILE__ ) . 'env.php' );
+	$verifier = new JWTVerifier([
+	  'supported_algs' => ['RS256'],
+	  'valid_audiences' => $env['valid_audiences'],
+	  'authorized_iss' => $env['authorized_iss'],
+	  'cache' => new FileSystemCacheHandler()
+	]);
+	return $verifier->verifyAndDecode($token);
+}
+
+function upsert_user_for_auth0($query) {
+	// Safety first
+	verify_token($query['token']);
+
+	// We fuck around with Auth0 functions that nobody undetstands
+  	$a0_options = WP_Auth0_Options::Instance();
+  	$user_repo = new WP_Auth0_UsersRepo( $a0_options );
+
+	$user_object = $query['user'];
+	$user_first_name = $user_object['user_metadata']['first_name'] ?: $user_object['given_name'];
+	$user_last_name = $user_object['user_metadata']['last_name'] ?: $user_object['family_name'];
+	slack_log($user_object);
+	// Does this user already exist?
+	$existing_user = get_user_by( 'email', $user_object['email'] );
+	if( $existing_user ) {
+		// A user with this email already exists, so we set the Auth0 data to whatever Auth0 sent along
+		  $user_repo->update_auth0_object( $existing_user->ID, (object)$user_object );
+	} else {
+		// No user with this email exists, so we create them and run Auth0's mystery functions
+		$password = wp_generate_password( 12, true );
+		$user_id = wp_create_user( $user_object['email'], $password, $user_object['email'] );
+		$user_repo->update_auth0_object( $user_id, (object)$user_object );
+	}
+
+	// Get the existing or newly created user
+	$wp_user = get_user_by_email( $user_object['email'] );
+	// We have certain variables we want to overwrite if the user has just signed up
+  	if($user_first_name) update_user_meta( $wp_user->ID, 'first_name', $user_first_name );
+  	if($user_last_name) update_user_meta( $wp_user->ID, 'last_name', $user_last_name );
+
+	// We want to send some of the user meta back to Auth0
+	$user_meta = get_user_meta($wp_user->ID);
+	$wp_user->meta = $user_meta;
+	return $wp_user;
+}
 
 add_action( 'rest_api_init', function() {
   register_rest_route('mw_auth0/v1', '/user/', array(
@@ -110,9 +156,9 @@ add_action( 'rest_api_init', function() {
     )
   ) );
 
-  register_rest_route('mw_auth0/v1', '/user_create/', array(
+  register_rest_route('mw_auth0/v1', '/upsert_user/', array(
   		'methods' => 'POST',
-		'callback' => 'create_user_for_auth0',
+		'callback' => 'upsert_user_for_auth0',
 		'args' => array(
 			'token' => array(
 				'required' => true,
